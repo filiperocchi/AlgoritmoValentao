6af3d863a5b7cffa4e02abffd0730e4ca18f5cdc
Filipe Santos Rocchi <filipe.rocchi@gmail.com>
2015-10-04 10:12:05 -0300

    ProjetoCriado
    Linguagem Java.
    Usando Netbeans.

---
+++ src/valentao/Node.java
@@ -0,0 +1,13 @@
+/*
+	UFSCar - BCC 2015-2 - Sistemas Distribuídos
+	Trabalho 3 - Algoritmo do valentão
+
+	Filipe Santos Rocchi 552194
+	Rafael Brandão Barbosa Fairbanks 552372
+*/
+
+package valentao;
+
+public class Node {
+	
+}
---
+++ src/valentao/Valentao.java
@@ -0,0 +1,22 @@
+/*
+	UFSCar - BCC 2015-2 - Sistemas Distribuídos
+	Trabalho 3 - Algoritmo do valentão
+
+	Filipe Santos Rocchi 552194
+	Rafael Brandão Barbosa Fairbanks 552372
+*/
+
+package valentao;
+
+public class Valentao {
+
+	public static void main(String[] args) {
+		if(1==1) System.out.println("Integer.MAX_VALUE: "+Integer.MAX_VALUE);
+		
+		// cria e inicia threads
+		
+		// chama shutdown da thread coordenador
+		
+	}
+	
+}
